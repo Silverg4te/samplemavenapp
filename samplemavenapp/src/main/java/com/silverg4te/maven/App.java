e6ac56d0ebcbb13a2da08f9e33bb887fbae0f765
Carl King <Silverg4te@users.noreply.github.com>
2020-08-22 00:23:23 +0800

    Basic Generated J2EE project using Maven Archetype

---
+++ samplemavenapp/src/main/java/com/silverg4te/maven/App.java
@@ -0,0 +1,13 @@
+package com.silverg4te.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
